52635b09c8322f2277021d1c00ea00c7e58ca863
kleinesfilmroellchen <malu.bertsch@gmail.com>
2018-12-27 18:12:14 +0100

    state improvements
    added longer message as information about which state means what

--- src/rpg/helpers/State.java
+++ src/rpg/helpers/State.java
@@ -1,19 +1,40 @@
 package rpg.helpers;
 
 /**
- * An enum for different results of methods.
- * Is used to find out how the method execution turned out.
+ * An enum for different results of methods. Is used to find out how the method
+ * execution turned out.
  * @author kleinesfilmröllchen
  * @since 0.0.0004
  * @version 0.0.0004
  */
 public enum State {
-	/**The finished state. Methods return this to indicate a completely normal execution.*/
-	finished,
-	/**The failed state. Methods return this when something not gameplay-related goes wrong but an exception throw is not necessary.*/
-	failed,
-	/**The not allowed state. Methods return this to indicate that the execution was not allowed due to game rules.<br>A message should be supplied to inform the user why the execution wasn't allowed.*/
-	notAllowed,
-	/**The early exit state. Methods return this to indicate a partially successful execution which was stopped by a "not allowed" type incident. Especially for methods which execute several independent actions.*/
-	earlyExit,
+	/**
+	 * The finished state. Methods return this to indicate a completely normal
+	 * execution.
+	 */
+	finished("Execution finished successfully"),
+	/**
+	 * The failed state. Methods return this when something not gameplay-related
+	 * goes wrong but an exception throw is not necessary.
+	 */
+	failed("Execution failed"),
+	/**
+	 * The not allowed state. Methods return this to indicate that the execution was
+	 * not allowed due to game rules.<br>
+	 * A message should be supplied to inform the user why the execution wasn't
+	 * allowed.
+	 */
+	notAllowed("This is not allowed!"),
+	/**
+	 * The early exit state. Methods return this to indicate a partially successful
+	 * execution which was stopped by a "not allowed" type incident. Especially for
+	 * methods which execute several independent actions.
+	 */
+	earlyExit("Execution stopped early but was partially successful");
+
+	public String message = "";
+
+	State(String msg) {
+		this.message = msg;
+	}
 }
